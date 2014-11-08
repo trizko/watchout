784e3c82ed5c8f77241b60be9125a2c468583e31
trizko <trizko>
2014-11-07 20:09:49 -0800

    Completed basic requirements

--- watchout.js
+++ watchout.js
@@ -1,10 +1,17 @@
 // start slingin' some d3 here.
 
+var game = {
+  high : 0,
+  current: 0,
+  collisions: 0
+}
+
 var width = 700;
 var height = 450;
+var radius = 10;
 
 //Define d3 canvas
-var svg = d3.select("body").append("svg")
+var svg = d3.select("body").append('div').attr("class", "container").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
@@ -48,7 +55,7 @@ var update = function(enemyData){
       .attr("cy", function(d, i){
         return Math.floor(Math.random()*height);
       })
-      .attr("r", 10)
+      .attr("r", radius)
 
 
 
@@ -71,6 +78,50 @@ setInterval(function(){
   update(enemyData);
 },2000);
 
+var updateScores = function(cls){
+  var element = document.getElementsByClassName(cls)[0].children[0];
+  element.innerHTML = game[cls].toString();
+}
+
+var collisions = function(){
+  var enemyArray = svg.selectAll(".asteroid")[0];
+  var player = svg.selectAll(".player")[0][0];
+
+  var playerR = player['r']['animVal']['value'];
+  var playerX = parseFloat(player['cx']['animVal']['value']);
+  var playerY = parseFloat(player['cy']['animVal']['value']);
+
+  for(var i = 0; i < enemyArray.length; i++){
+    var enemyR = radius;
+    var enemyX = parseFloat(enemyArray[i]['cx']['animVal']['value']);
+    var enemyY = parseFloat(enemyArray[i]['cy']['animVal']['value']);
+
+    radiusSum = enemyR + playerR;
+    xDiff = enemyX - playerX;
+    yDiff = enemyY - playerY;
+
+    separation = Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff,2));
+    if(separation < radiusSum){
+      if(game.high < game.current){
+        game.high = game.current;
+        updateScores("high");
+      }
+      game.current = 0;
+      updateScores("current");
+      game.collisions++;
+      updateScores("collisions");
+      console.log("this is a hit");
+    }
+
+  }
+}
+
+setInterval(function(){
+  collisions();
+  game.current++;
+  updateScores('current');
+},200);
+
 //Create Player
 var playerData = [0];
 var player = svg.selectAll('.player')
@@ -78,7 +129,7 @@ var player = svg.selectAll('.player')
 
 player.enter().append("circle")
       .attr("class", "player")
-      .attr("cx", width/2 - 50)
-      .attr("cy", height/2 - 10)
-      .attr("r", 10)
+      .attr("cx", width/2)
+      .attr("cy", height/2)
+      .attr("r", radius)
       .call(drag);
